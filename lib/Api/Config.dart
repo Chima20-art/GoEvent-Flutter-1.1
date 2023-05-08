e8436771aa84ef146e8a12490a92e75fdc63478e
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-05-08 18:58:08 +0100

    first commit

---
+++ lib/Api/Config.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: file_names
+
+class Config {
+  static const String oneSignel = '83bc1524-d9e0-437f-93ca-e955291f0a52';
+
+  static const String userImage =
+      "http://192.168.1.20:8000/images/profile/pic.png";
+  static const String imageURLPath = 'http://192.168.1.20:8000/';
+  static const String baseurl = 'http://192.168.1.20:8000/eapi/';
+  static const String reguser = 'e_reg_user.php';
+  static const String mobilecheck = 'e_mobile_check.php';
+  static const String pagelist = 'e_pagelist.php';
+  static const String cCode = 'e_country_code.php';
+  static const String loginuser = 'e_login_user.php';
+  static const String forgetpassword = 'e_forget_password.php';
+
+  static const String homedat = "e_home_data.php";
+  static const String couponlist = 'e_couponlist.php';
+  static const String paymentgateway = 'e_paymentgateway.php';
+  static const String checkcoupon = 'e_check_coupon.php';
+  static const String profileedit = 'pro_image.php';
+  static const String profil = 'e_profile_edit.php';
+
+  static const String faq = 'e_faq.php';
+  static const String walletreport = 'e_wallet_report.php';
+  static const String walletup = 'e_wallet_up.php';
+  static const String egetdata = 'e_getdata.php';
+  static const String eventdataApi = "e_event_data.php";
+  static const String ebookmark = "e_bookmark.php";
+  static const String bookmarkApi = "e_bookmark_list.php";
+  static const String typePrice = "e_get_type_price.php";
+  static const String ebookticket = "e_book_ticket.php";
+  static const String ticketpreview = "e_ticket_preview.php";
+  static const String eventSearch = "e_event_search.php";
+  static const String catEvent = "e_cat_event.php";
+  static const String bookticket = "e_book_ticket_data.php";
+  static const String ticketCancel = "e_ticket_cancle.php";
+  static const String ticketStatus = "e_ticket_status_wise.php";
+  static const String ticketdata = "e_ticket_data.php";
+  static const String refardata = "e_getdata.php";
+  static const String rateUpdate = "e_rate_update.php";
+  static const String notification = "e_notification_list.php";
+}
